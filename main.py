b4b31a19ae8c1c585d3eca0c950c47276008290f
mkripinski <164096839+mkripinski@users.noreply.github.com>
2025-03-01 16:16:07 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,38 +1,54 @@
 import streamlit as st
 from google import genai
 client = genai.Client(api_key="AIzaSyBFB5rg_NNHDzvbfV0dGlukpK5ViHNR4fI")
-chat = client.chats.create(model="gemini-2.0-flash")
 
+#Prompt definitions
+prompts = [
+    "Enter First Prompt Here",
+    "Enter Second Prompt Here"
 
+]
 
+def clear_history():
+     st.session_state.chat = client.chats.create(model="gemini-2.0-flash",history=list())
+
+st.set_page_config(
+    page_title="King's Pigeon",
+)
+
+#Session state variable declaration
+if 'chat' not in st.session_state:
+    st.session_state.chat = client.chats.create(model="gemini-2.0-flash",history=list())
+
+if 'prompt_selection' not in st.session_state:
+    st.session_state.prompt_selection = 1
+#Page Title
 st.title("King's Secret Helper")
 
-if 'messages' not in st.session_state:
-    st.session_state.messages = [{"role": "assistant", 
-                 "content": "Greetings my King. How can I be of assistance?"},]
-# We loop through each message in the session state and render it as
-# a chat message.
-for message in st.session_state.messages:
-    with st.chat_message(message['role']):
-        st.markdown(message["content"],)
+with st.chat_message("assistant"):
+        st.markdown("Greetings my King. How can I be of assistance?")
 
+#prompt selection cases
+match st.session_state.prompt_selection:
 
+        case 1:
+
+            st.session_state.chat.send_message(prompts[1])
+        case 2:
+            st.session_state.chat.send_message(prompts[2])
+        case _:
+            st.warning("Ye Must Choose Ye Chat Type")
+
+#User Enters Text
 if user_prompt := st.chat_input("Enter Ye Decree Hence"):
-    # Add our input to the session state
-    st.session_state.messages.append(
-        {"role": "user", "content": user_prompt}
-    )
-    
+   
     # Add our input to the chat window
     with st.chat_message("user"):
         st.markdown(user_prompt)
     
-    response = chat.send_message(user_prompt)
+    #send input to Gemini and retrieve response
+    response = st.session_state.chat.send_message(user_prompt)
 
-    st.session_state.messages.append(
-        {'role': "assistant",
-         "content": response.text}
-    )
-
+    #add response to message box
     with st.chat_message("assistant"):
         st.markdown(response.text)
