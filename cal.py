1708838f1feb36366d17f771c7230443517c6c95
Jaairg <jair.02.14@outlook.com>
2025-03-02 01:22:27 -0500

    Update cal.py

--- cal.py
+++ cal.py
@@ -1,3 +1,8 @@
 import streamlit as st
 
-st.title("Calendar")
+st.title("📅 Calendar")
+
+
+calendar_url = "https://calendar.google.com/calendar/embed?src=fdf244d3bad05b417aae9441b4a141be7ba3edaba0d2f2a295eef2768cc6656d%40group.calendar.google.com&ctz=America%2FNew_York"
+
+st.components.v1.iframe(calendar_url, width=800, height=600)
