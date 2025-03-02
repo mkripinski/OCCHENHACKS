f0d0d058f480e3fa0a5f5f4d5f5cfbcc0eb8bd7c
bird-d <barbaragrusky123@gmail.com>
2025-03-02 09:19:12 -0500

    Update note.py

--- note.py
+++ note.py
@@ -1,17 +1,17 @@
 import streamlit as st
 
-st.title("Notes")
+st.title("              Notes")
 tab1, tab2, tab3 = st.tabs(["English", "Herbology", "Math"])
 with tab1:
-    st.header("English Notes")
+    st.header("English Notes :books:")
 user_input = st.text_area("Notes")
 
 with tab2:
-    st.header("Herbology")
+    st.header("Herbology Notes :seedling:")
     
 
 with tab3:
-    st.header("Math")
+    st.header("Math Notes :blue_book:")
     
 
 
